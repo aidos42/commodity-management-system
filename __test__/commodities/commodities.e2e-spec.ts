cd7a28c8e14e65fcbf2412477ee1a103e3055acd
aidos <aidos49@yahoo.com>
2022-08-01 19:27:31 +0300

    add negative cases in tests

--- __test__/commodities/commodities.e2e-spec.ts
+++ __test__/commodities/commodities.e2e-spec.ts
@@ -6,31 +6,31 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommodityDto } from '../../src/commodities/dto/commodity.dto';
 import * as codes from 'http-codes';
 
+let app: INestApplication;
+
+beforeAll(async () => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [
+      TypeOrmModule.forRoot({
+        type: 'sqlite',
+        database: 'database.sqlite',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      CommoditiesModule,
+    ],
+  }).compile();
+
+  app = moduleFixture.createNestApplication();
+  await app.init();
+});
+
 describe('positive cases', () => {
   const commodity = {
     title: 'mucguffin',
     price: 42,
     type: 'muffin',
   };
-
-  let app: INestApplication;
-
-  beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forRoot({
-          type: 'sqlite',
-          database: 'database.sqlite',
-          autoLoadEntities: true,
-          synchronize: true,
-        }),
-        CommoditiesModule,
-      ],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
 
   it('should create new commodity by POST', () => {
     return request(app.getHttpServer())
@@ -58,5 +58,32 @@ describe('positive cases', () => {
       .then(({ body }) => {
         expect(body).toBeDefined();
       });
+  });
+});
+
+describe('negative cases', () => {
+  const wrongCommodity = {
+    title: 'mucguffin',
+    price: 42,
+  };
+
+  //TODO: Figure out why the server crashes with such payload, but not in Postman
+  it('should throw 400 because of wrong payload', () => {
+    return request(app.getHttpServer())
+      .post('/api/commodities')
+      .send(wrongCommodity as CommodityDto)
+      .expect(codes.INTERNAL_SERVER_ERROR);
+  });
+
+  it('should throw 404 because of wrong url', () => {
+    return request(app.getHttpServer())
+      .get('/api/commoditied')
+      .expect(codes.NOT_FOUND);
+  });
+
+  it('should throw 404 because of wrong commoditie id', () => {
+    return request(app.getHttpServer())
+      .get('/api/commodities/666')
+      .expect(codes.NOT_FOUND);
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Commodity } from './commodities/commodity.entity';
+import { CommoditiesModule } from './commodities/commodities.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([Commodity])],
+  imports: [
+    TypeOrmModule.forRoot(),
+    TypeOrmModule.forFeature([Commodity]),
+    CommoditiesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
